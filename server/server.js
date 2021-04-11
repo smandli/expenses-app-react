e40a1ca9ffbbf338bf488740459ba099e3c3ca98
Sebastian Mandli <sebastianmandli@gmail.com>
2021-04-11 14:40:39 +1000

    chore: setup production build and server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const path = require('path');
-var express = require('express')
+const express = require('express')
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public')
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get("/", (req, res) => {
-    res.send("Test");
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
 })
 
 app.listen(port, () => {
