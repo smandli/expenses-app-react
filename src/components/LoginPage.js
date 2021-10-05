becb1441ed9b9509b3fb88a6528a845a165e39af
smandli <sebastianmandli@gmail.com>
2021-10-05 19:41:34 +1100

    feat: Update Google button

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,9 @@ export const LoginPage = ({ startLogin }) => (
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expenses app</h1>
             <p>Get your expenses under control.</p>
-            <button onClick={startLogin} className="button">Login with Google</button>
+            <button onClick={startLogin} className="auth-button">
+                <img src="/images/btn_google_signin.png"></img>
+            </button>
         </div>
     </div>
 )
